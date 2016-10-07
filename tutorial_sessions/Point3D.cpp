9eb2244454d21e2c3b7784f66d7b97d7121ba505
AJ Ibraheem <gai10@uni.brighton.ac.uk>
2016-10-07 12:29:38 +0100

    Update to the Point3D class

--- tutorial_sessions/Point3D.cpp
+++ tutorial_sessions/Point3D.cpp
@@ -11,9 +11,24 @@ Point3D::Point3D(float x, float y, float z){
     Point3D::z = z;
 }
 
-Point3D::~Point3D(){}
+Point3D::~Point3D(){
+}
 
+//Getter implementations
 float Point3D::getX(){ return x; }
 float Point3D::getY(){ return y; }
 float Point3D::getZ(){ return z; }
 string Point3D::getColor(){ return color; }
+
+//Setter implementations
+void Point3D::setX(float x) {
+    Point3D::x = x;
+}
+
+void Point3D::setY(float y) {
+    Point3D::y = y;
+}
+
+void Point3D::setZ(float z) {
+    Point3D::z = z;
+}
--- tutorial_sessions/Point3D.h
+++ tutorial_sessions/Point3D.h
@@ -12,10 +12,16 @@ class Point3D {
 public:
     Point3D(float x, float y, float z);
     ~Point3D();
+    //Getters
     float getX();
     float getY();
     float getZ();
     string getColor();
+    //Setters
+    void setX(float);
+    void setY(float);
+    void setZ(float);
+    void setColor(string);
 
 private:
     float x;
