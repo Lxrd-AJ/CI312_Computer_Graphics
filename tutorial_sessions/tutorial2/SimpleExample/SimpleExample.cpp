5948be3afd8e5d0a94fccb93778f6334676bef90
AJ Ibraheem <gai10@uni.brighton.ac.uk>
2016-10-21 09:50:42 +0100

    Tutorials 2 added

---
+++ tutorial_sessions/tutorial2/SimpleExample/SimpleExample.cpp
@@ -0,0 +1,28 @@
+// SimpleExample 
+//
+// This tutorial shows a simple example of 
+// how to use OpenSG together with GLUT to create a little
+// interactive scene viewer.
+// taken from OpenSG Tutorials
+
+
+// headers for OpenSG configuration and GLUT
+#include <OpenSG/OSGGLUT.h>
+#include <OpenSG/OSGConfig.h>
+#include <OpenSG/OSGSimpleGeometry.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGSimpleSceneManager.h>
+#include <OpenSG/OSGSceneFileHandler.h>
+#include <OpenSG/OSGGeoProperties.h>
+#include <math.h>
+
+// Simple Scene manager for accesing cameras and geometry
+OSG::SimpleSceneManagerRefPtr mgr;
+
+
+
+
+int main(int argc, char **argv)
+{
+
+}
